8e4e19e6b5c1776272bdacbcebd8d10e3c2c6487
yhc <okyhc@163.com>
2019-04-02 15:14:21 +0800

    重构消息中心

---
+++ rpc/msgcenter.go
@@ -0,0 +1,72 @@
+package rpc
+
+import (
+	"fmt"
+	"hash/fnv"
+	"reflect"
+
+	"github.com/protobuf/proto"
+)
+
+/*MsgHandler 是个消息回调函数，需要实现，并注册到MsgCenter
+  target 消息来源
+  msg 消息本体
+  stream grpc流，用于回复*/
+type MsgHandler func(target int32, msg interface{}, stream interface{})
+
+type msgInfo struct {
+	elem    reflect.Type
+	handler MsgHandler
+}
+
+/*MsgCenter 消息中心,封装消息工具*/
+type MsgCenter struct {
+	hash map[string]int32
+	info map[int32]msgInfo
+}
+
+/*NewMsgCenter 新消息中心*/
+func NewMsgCenter() *MsgCenter {
+	return &MsgCenter{hash: make(map[string]int32), info: make(map[int32]msgInfo)}
+}
+
+/*Reg 消息注册
+  msg 消息定义
+  handler 消息处理函数*/
+func (mi *MsgCenter) Reg(msg interface{}, handler MsgHandler) {
+	name := reflect.TypeOf(msg.(proto.Message)).Elem().Name()
+	h := fnv.New32a()
+	h.Write([]byte(name))
+	hh := int32(h.Sum32())
+	if info, ok := mi.info[hh]; ok {
+		panic(fmt.Errorf("Hash(%d) conflict %s %s", hh, name, info.elem.Name()))
+	}
+	mi.info[hh] = msgInfo{reflect.TypeOf(msg.(proto.Message)).Elem(), handler}
+	mi.hash[name] = hh
+}
+
+/*Wrap 将消息封装为转发消息
+  target 转发目标
+  msg 被编码的消息*/
+func (mi *MsgCenter) Wrap(target int32, msg proto.Message) (*ForwardMsg, error) {
+	if hash, ok := mi.hash[reflect.TypeOf(msg.(proto.Message)).Elem().Name()]; ok {
+		buf, err := proto.Marshal(msg)
+		return &ForwardMsg{Target: target, Msg: &RawMsg{Type: hash, Raw: buf}}, err
+	}
+	return nil, fmt.Errorf("%s don't register", reflect.TypeOf(msg.(proto.Message)).Elem().Name())
+}
+
+/*Handle 转发消息处理函数
+  fmsg 转发消息
+  stream 消息流*/
+func (mi *MsgCenter) Handle(fmsg *ForwardMsg, stream interface{}) error {
+	if info, ok := mi.info[fmsg.Msg.Type]; ok {
+		msg := reflect.New(info.elem).Interface()
+		err := proto.Unmarshal(fmsg.Msg.Raw, msg.(proto.Message))
+		if err == nil {
+			info.handler(fmsg.Target, msg, stream)
+		}
+		return err
+	}
+	return fmt.Errorf("invalid message type %d", fmsg.Msg.Type)
+}
--- rpc/service.go
+++ rpc/service.go
@@ -8,17 +8,20 @@ import (
 	"google.golang.org/grpc"
 )
 
+// ServerService rpc服务器端封装
 type ServerService struct {
-	Addr   string
-	Typeid int32
+	Addr      string //服务器地址
+	Typeid    int32  //服务器类型
+	Msgcenter *MsgCenter
 }
 
+// Forward rpc.Forward调用实现,解析转发来的消息
 func (ss *ServerService) Forward(stream Server_ForwardServer) (err error) {
 	var in *ForwardMsg
 	for {
 		in, err = stream.Recv()
 		if err == nil {
-			err = ForwardMsgHandle(in, stream)
+			err = ss.Msgcenter.Handle(in, stream)
 		}
 		if err != nil {
 			return
@@ -26,6 +29,7 @@ func (ss *ServerService) Forward(stream Server_ForwardServer) (err error) {
 	}
 }
 
+// Run 运行rpc服务,阻塞的
 func (ss *ServerService) Run() error {
 	lis, err := net.Listen("tcp", ss.Addr)
 	if err == nil {
@@ -36,20 +40,24 @@ func (ss *ServerService) Run() error {
 	return err
 }
 
+// ClientService rpc客户端服务
 type ClientService struct {
-	Addr   string
-	Typeid int32
+	Addr   string //服务器地址
+	Typeid int32  //服务器类型
 	stream Server_ForwardClient
 }
 
+// Stream 服务器流
 func (cs *ClientService) Stream() Server_ForwardClient {
 	return cs.stream
 }
 
+// TypeID 服务器类型
 func (cs *ClientService) TypeID() int32 {
 	return cs.Typeid
 }
 
+// Init 初始化
 func (cs *ClientService) Init() error {
 	conn, err := grpc.Dial(cs.Addr, grpc.WithInsecure())
 	if err == nil {
